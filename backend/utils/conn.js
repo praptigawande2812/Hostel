42e98cbe834b496cab25c41972f5dcf32c342918
Prapti Gawande <prapting2812@gmail.com>
2025-08-09 11:07:56 +0530

    atlas

--- backend/utils/conn.js
+++ backend/utils/conn.js
@@ -5,14 +5,16 @@ const mongoURI = process.env.MONGO_URI;
 const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: 'hostel' // Explicitly specify database name
         });
         console.log('MongoDB connection SUCCESS');
     } catch (error) {
         console.error('MongoDB connection FAIL');
+        console.error('Error details:', error.message);
         process.exit(1);
     }
-    };
+};
 
 module.exports = connectDB;
